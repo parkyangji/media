f0844c1e7cad6314eee0c4dc99f9b9c7a214af58
parkyangji <dhsfid02@gmail.com>
2023-04-26 17:11:49 +0900

    sub1

---
+++ js/sub_visual.js
@@ -0,0 +1,30 @@
+// 스크린
+var screenSize, screenHeight;
+
+function screen_size(){
+    screenSize = $(window).width(); //스크린의 너비
+    screenHeight = $(window).height();  //스크린의 높이
+
+    $("#content").css('margin-top',screenHeight);
+    
+    if( screenSize > 1024){
+        $("#imgBG").attr("src", "./images/sub1_main1.jpg");
+      }
+    if(screenSize <= 1024){
+      $("#imgBG").attr("src", "./images/sub1_main1_2.jpg");
+    }
+}
+
+screen_size();  //최초 실행시 호출
+
+$(window).resize(function(){ 
+    screen_size();
+}); 
+
+// 스크롤 다운
+  
+$('.scrollDown').click(function(){
+  screenHeight = $(window).height();
+  $('html,body').animate({'scrollTop':screenHeight}, 1000);
+});
+
