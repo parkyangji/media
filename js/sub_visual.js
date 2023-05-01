94a7b20fa45ecaa8fa7f675478df44f33c98b731
parkyangji <dhsfid02@gmail.com>
2023-05-01 15:42:05 +0900

    sub2

--- js/sub_visual.js
+++ js/sub_visual.js
@@ -1,5 +1,18 @@
 // 스크린
-var screenSize, screenHeight;
+
+function getParams() {
+  let url = decodeURIComponent(location.href);
+  url = decodeURIComponent(url); // http://127.0.0.1:5500/sub2.html
+
+  let params = '';
+  params = url.substring(url.indexOf("sub")+3, url.indexOf("sub")+4)
+
+  return params;
+}
+
+let pageNum = getParams(); // 페이지 수 뽑기
+
+let screenSize, screenHeight;
 
 function screen_size(){
     screenSize = $(window).width(); //스크린의 너비
@@ -8,10 +21,10 @@ function screen_size(){
     $("#content").css('margin-top',screenHeight);
     
     if( screenSize > 1024){
-        $("#imgBG").attr("src", "./images/sub1_main1.jpg");
+        $("#imgBG").attr("src", `./images/sub${pageNum}_main1.jpg`);
       }
     if(screenSize <= 1024){
-      $("#imgBG").attr("src", "./images/sub1_main2.JPG");
+      $("#imgBG").attr("src", `./images/sub${pageNum}_main2.JPG`);
     }
 }
 
